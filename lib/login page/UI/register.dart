d2f4a03bbd12f58f3b11cdef0284a497bd76330d
Nithin Mahendran <nithinmahendren@gmail.com>
2021-06-24 12:49:43 +0530

    Cleared main file

--- lib/login page/UI/register.dart
+++ lib/login page/UI/register.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:loginapp/models/global.dart';
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({ Key? key }) : super(key: key);
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
   _RegisterPageState createState() => _RegisterPageState();
@@ -10,8 +11,22 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Text(
+                  "Sign Up",
+                  style: headerTitle,
+                ),
+                Text("Enter your details to continue.", style: descriptionText),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/models/global.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+TextStyle headerTitle = TextStyle(fontSize: 25.0, color: Colors.black);
+
+TextStyle descriptionText = TextStyle(fontSize: 15.0, color: Colors.black);
