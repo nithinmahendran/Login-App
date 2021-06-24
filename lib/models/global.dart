0e5bacef4ae45b49ec9b329fe0be6358b5187c84
Nithin Mahendran <nithinmahendren@gmail.com>
2021-06-24 15:47:43 +0530

    Sign Up page completed

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -2,6 +2,16 @@ import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 
-TextStyle headerTitle = TextStyle(fontSize: 25.0, color: Colors.black);
+TextStyle headerTitle = const TextStyle(
+    fontSize: 55.0, color: Colors.black, fontWeight: FontWeight.w600);
 
-TextStyle descriptionText = TextStyle(fontSize: 15.0, color: Colors.black);
+TextStyle descriptionText =
+    const TextStyle(fontSize: 18.0, color: Colors.black);
+
+Color textField = const Color(0xffEDEDED);
+
+TextStyle buttonText = const TextStyle(
+  fontSize: 18.0,
+  color: Colors.white,
+  fontWeight: FontWeight.w400
+);
